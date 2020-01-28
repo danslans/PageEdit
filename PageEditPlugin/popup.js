d59142d6b4a850bb24a3b88e51d88c0687b11e6c
danslans <danslans@MacBook-Air-de-danslans.local>
2020-01-28 07:53:13 -0500

    Agrega configuración de ApiDark

--- PageEditPlugin/popup.js
+++ PageEditPlugin/popup.js
@@ -1,15 +1,15 @@
-var styles=["size=200"];
-
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
     if (changeInfo.status == 'complete' && tab.active) {
-        console.log("content")
+        console.log("content");
+        let edit = document.getElementById("edit");
+        let cancel = document.getElementById("editOff");
+        if(edit.addEventListener && cancel.addEventListener){
+            edit.addEventListener("click",event=>{
+                chrome.storage.sync.set({"state":"on"});
+            },false);
+            cancel.addEventListener("click",event=>{
+                chrome.storage.sync.set({"state":"off"});
+            },false);
+        }
     }
   });
-
-  document.getElementById("edit").addEventListener("click",function(event){
-    chrome.storage.sync.set({"state":"on"});
-},false);
-document.getElementById("editOff").addEventListener("click",function(event){
-    chrome.storage.sync.set({"state":"off"});
-},false);
-;

32f6c95e571da3ec58a2156c0eece23bc205048a
daniel.gomez <daniel.gomez@tecnofactor.com>
2020-01-28 11:07:04 -0500

    Agrega cambio en estructura

--- PageEditPlugin/popup.js
+++ PageEditPlugin/popup.js
@@ -1,15 +1,16 @@
-chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     if (changeInfo.status == 'complete' && tab.active) {
         console.log("content");
-        let edit = document.getElementById("edit");
-        let cancel = document.getElementById("editOff");
-        if(edit.addEventListener && cancel.addEventListener){
-            edit.addEventListener("click",event=>{
-                chrome.storage.sync.set({"state":"on"});
-            },false);
-            cancel.addEventListener("click",event=>{
-                chrome.storage.sync.set({"state":"off"});
-            },false);
-        }
     }
-  });
+});
+
+let edit = document.getElementById("edit");
+let cancel = document.getElementById("editOff");
+if (edit.addEventListener && cancel.addEventListener) {
+    edit.addEventListener("click", event => {
+        chrome.storage.sync.set({ "state": "on" });
+    }, false);
+    cancel.addEventListener("click", event => {
+        chrome.storage.sync.set({ "state": "off" });
+    }, false);
+}

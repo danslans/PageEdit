02a1cdff7a5b54d08d249ce18b7672418c7cee5c
danslans <danslans@MacBook-Air-de-danslans.local>
2020-01-28 22:26:48 -0500

    agrega prueba de color

--- PageEditPlugin/myscript.js
+++ PageEditPlugin/myscript.js
@@ -9,14 +9,16 @@ document,addEventListener("load",function(event){
     chrome.storage.sync.set(JSON.parse(jsonToSave));
 },false);
 
-document.addEventListener("click",function(event){
+document.addEventListener("mousemove",function(event){
     debugger;
     chrome.storage.sync.get("state",function(value){
         if(value.state=="on"){
             var elementClick = event.toElement;
-            elementClick.style.backgroundColor = "red";
+            elementClick.style.backgroundColor = "rgba(144, 215, 249, 0.16)";
+            elementClick.style.boxShadow="1px 2px #ccd5ff66";
             var menu = document.getElementById("u_0_a");
             console.log("holi");
         }
     });
 },false);
+
